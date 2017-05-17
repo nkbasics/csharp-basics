1313c30d17274df470099f783c3683a5af02cd28
KERNGRUPPE\nk.DIVI01 <matthias.bruch@netzkern.de>
2017-05-18 00:02:04 +0200

    Demo1

---
+++ DemoCode1/DemoCode1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace DemoCode1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string test = "abc";
+
+        }
+    }
+    
+}
